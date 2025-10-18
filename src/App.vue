333c6f1a8b782fb0d5d39c6a8bec751127be0001
unknown <antonmg91@hotmail.com>
2025-10-18 10:46:40 +0200

    Changes10.46

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<main>
+  <h1>Ironcontacts</h1>
+  
+  <button @click="addRandomcontact">Add Random Contact</button>
+  <button @click="SortByPopularity">Sort by Popularity</button>
+  <button @click="SortByName">Sort by Name</button>
+
+    
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+        </tr>
+      </thead>
+    <tbody>
+    <tr v-for="contact in contacts" :key="contact.id">
+      <td><img :src="contact.pictureUrl" :alt="`Photo of ${contact.name}`" width="80" /></td>             
+      <td>{{ contact.name }}</td>
+      <td> {{ contact.popularity }}</td>
+      <td><span v-if="contact.wonOscar">🏆</span></td>
+      <td><span v-if="contact.wonEmmy">🏆</span></td>  
+    </tr>
+    </tbody>
+    </table>
+  </main>
+
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
+import contactsData from './contacts.json'
+
+const contacts = ref(contactsData.slice(0, 6))
+
+function addRandomcontact() {
+      const remainingContacts = contactsData.filter(
+        c => !contacts.value.includes(c)
+      );
+      if (remainingContacts.length > 0) {
+        const randomIndex = Math.floor(Math.random() * remainingContacts.length);
+        const randomContact = remainingContacts[randomIndex];
+
+        contacts.value.push(randomContact);
+        } else {
+          alert("No more contacts to add");
+        }
+      }
+    
+function SortByPopularity () {
+  contacts.value.sort((a, b) => b.popularity - a.popularity);
+}
+
+function SortByName() {
+  contacts.value.sort((a, b) => a.name.localeCompare(b.name));
+}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+td {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+}
+
+button {
+  display: block;
+  margin: 20px auto
+  
 }
 </style>
